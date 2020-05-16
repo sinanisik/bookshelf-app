81485727a6defaff87b22d905c7390ed7b650bbf
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2020-05-16 20:11:44 +0300

    add hooks

--- src/containers/AddBook/AddBook.js
+++ src/containers/AddBook/AddBook.js
@@ -1,44 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AddBookForm } from "../../components";
 import { Container, Row, Col } from "reactstrap";
 import axios from "axios";
 import { apiHost } from "../../constants/constants";
 
-class AddBook extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      book: "",
-    };
-  }
+const AddBook = (props) => {
+  const [book, setBook] = useState("");
 
-  componentDidMount() {
-    const id = this.props.routerProps.match.params.id;
+  useEffect(() => {
+    const id = props.routerProps.match.params.id;
     if (id) {
       axios.get(`${apiHost}/books/${id}`).then((result) => {
-        this.setState({
-          book: result.data,
-        });
+        setBook(result.data);
       });
     }
-  }
+  }, []);
 
-  render() {
-    const isEdit = this.props.componentProps.isEdit;
-    const id = this.props.routerProps.match.params.id;
-    return (
-      <Container>
-        <Row>
-          <Col xs={12}>
-            {isEdit ? <h3>Edit book</h3> : <h3>Add a new book</h3>}
-          </Col>
-          <Col xs={12}>
-            <AddBookForm isEdit={isEdit} book={this.state.book} />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+  const isEdit = props.componentProps.isEdit;
+  const id = props.routerProps.match.params.id;
+  return (
+    <Container>
+      <Row>
+        <Col xs={12}>
+          {isEdit ? <h3>Edit book</h3> : <h3>Add a new book</h3>}
+        </Col>
+        <Col xs={12}>
+          <AddBookForm isEdit={isEdit} book={book} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default AddBook;
--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -1,101 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { Books, FilterSortBar, BookList } from "../../components";
 import { getBooks } from "../../state/ducks/books/actions";
 import { Container } from "reactstrap";
 import { selectFilteredBooks } from "../../state/ducks/books/reducers";
 
-class Homepage extends React.Component {
-  constructor(props) {
-    super(props);
+const Homepage = (props) => {
+  useEffect(() => {
+    props.getBooks();
+  }, []);
+
+  if (props.loading) {
+    return <h1>BOOKS ARE LOADING...</h1>;
   }
 
-  componentDidMount() {
-    this.props.getBooks();
-  }
+  let filteredBooks = [];
+  filteredBooks = props.filteredBooks;
 
-  render() {
-    if (this.props.loading) {
-      return <h1>BOOKS ARE LOADING...</h1>;
+  // Categorye gore filter
+  // const allBooks = this.props.books;
+  // if (this.props.filter) {
+  //   filteredBooks = allBooks.filter((book) => {
+  //     return book.category === this.props.filter;
+  //   });
+  // } else {
+  //   filteredBooks = allBooks;
+  // }
+
+  /// Searchterm filter
+  if (props.searchTerm) {
+    if (props.searchType === "title") {
+      filteredBooks = filteredBooks.filter((book) => {
+        return (
+          book.title.toLowerCase().indexOf(props.searchTerm.toLowerCase()) > -1
+        );
+      });
+    } else {
+      filteredBooks = filteredBooks.filter((book) => {
+        return (
+          book.author.toLowerCase().indexOf(props.searchTerm.toLowerCase()) > -1
+        );
+      });
     }
+  }
 
-    let filteredBooks = [];
-    filteredBooks = this.props.filteredBooks;
-
-    // Categorye gore filter
-    // const allBooks = this.props.books;
-    // if (this.props.filter) {
-    //   filteredBooks = allBooks.filter((book) => {
-    //     return book.category === this.props.filter;
-    //   });
-    // } else {
-    //   filteredBooks = allBooks;
-    // }
-
-    /// Searchterm filter
-    if (this.props.searchTerm) {
-      if (this.props.searchType === "title") {
-        filteredBooks = filteredBooks.filter((book) => {
-          return (
-            book.title
-              .toLowerCase()
-              .indexOf(this.props.searchTerm.toLowerCase()) > -1
-          );
-        });
-      } else {
-        filteredBooks = filteredBooks.filter((book) => {
-          return (
-            book.author
-              .toLowerCase()
-              .indexOf(this.props.searchTerm.toLowerCase()) > -1
-          );
-        });
+  // Sorting
+  let sortedBooks = [];
+  if (props.sortTerm) {
+    sortedBooks = [...filteredBooks].sort((book1, book2) => {
+      if (props.sortTerm === "ratingD") {
+        return parseInt(book2["rating"]) - parseInt(book1["rating"]);
+      } else if (props.sortTerm === "ratingA") {
+        return parseInt(book1["rating"]) - parseInt(book2["rating"]);
+      } else if (props.sortTerm === "alphabetical") {
+        return book1["title"].split(" ")[0] > book2["title"].split(" ")[0]
+          ? 1
+          : -1;
       }
-    }
-
-    // Sorting
-    let sortedBooks = [];
-    if (this.props.sortTerm) {
-      sortedBooks = [...filteredBooks].sort((book1, book2) => {
-        if (this.props.sortTerm === "ratingD") {
-          return parseInt(book2["rating"]) - parseInt(book1["rating"]);
-        } else if (this.props.sortTerm === "ratingA") {
-          return parseInt(book1["rating"]) - parseInt(book2["rating"]);
-        } else if (this.props.sortTerm === "alphabetical") {
-          return book1["title"].split(" ")[0] > book2["title"].split(" ")[0]
-            ? 1
-            : -1;
-        }
-      });
-    } else {
-      sortedBooks = filteredBooks;
-    }
-
-    let books = "";
-    if (sortedBooks.length < 1) {
-      books = <h2>NO BOOKS HERE</h2>;
-    } else {
-      books = <Books items={sortedBooks} view={this.props.view} />;
-    }
-
-    // Read Books Route
-    if (this.props.componentProps.isReadBooks) {
-      let readBooks = sortedBooks.filter((book) => {
-        return book.status === "Read";
-      });
-      books = <Books items={readBooks} view={this.props.view} />;
-    }
-
-    return (
-      <div>
-        <FilterSortBar />
-        <Container>
-          <div className="books-holder">{books}</div>
-        </Container>
-      </div>
-    );
+    });
+  } else {
+    sortedBooks = filteredBooks;
   }
-}
+
+  let books = "";
+  if (sortedBooks.length < 1) {
+    books = <h2>NO BOOKS HERE</h2>;
+  } else {
+    books = <Books items={sortedBooks} view={props.view} />;
+  }
+
+  // Read Books Route
+  if (props.componentProps.isReadBooks) {
+    let readBooks = sortedBooks.filter((book) => {
+      return book.status === "Read";
+    });
+    books = <Books items={readBooks} view={props.view} />;
+  }
+
+  return (
+    <div>
+      <FilterSortBar />
+      <Container>
+        <div className="books-holder">{books}</div>
+      </Container>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   console.log(state.books);

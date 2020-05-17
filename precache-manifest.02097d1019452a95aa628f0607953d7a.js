c5c856100edfe082990d136007f165a3a9fd9321
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2020-05-17 19:48:27 +0300

    Updates

---
+++ precache-manifest.02097d1019452a95aa628f0607953d7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54072756d58d6034f78033339e3b6c47",
+    "url": "/bookshelf-app/index.html"
+  },
+  {
+    "revision": "a76ed95f633678f60caa",
+    "url": "/bookshelf-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d866a39b8f69245dd1b8",
+    "url": "/bookshelf-app/static/css/main.aeabc570.chunk.css"
+  },
+  {
+    "revision": "a76ed95f633678f60caa",
+    "url": "/bookshelf-app/static/js/2.a389eb21.chunk.js"
+  },
+  {
+    "revision": "e0546182649f50a2eb923c2a881ed896",
+    "url": "/bookshelf-app/static/js/2.a389eb21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d866a39b8f69245dd1b8",
+    "url": "/bookshelf-app/static/js/main.d093bbc9.chunk.js"
+  },
+  {
+    "revision": "5d55b1b73b22e029a96c",
+    "url": "/bookshelf-app/static/js/runtime-main.50ccaa34.js"
+  }
+]);
